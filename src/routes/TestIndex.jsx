ce3a38c2ff2f9200c73fd152b8e920645ddbe488
LeeJY97 <ijoonyeol97@gmail.com>
2024-09-10 22:06:54 +0900

    [test] vercel

--- src/routes/TestIndex.jsx
+++ src/routes/TestIndex.jsx
@@ -1,13 +1,23 @@
-import SignUp from "/src/pages/sign/SignUp";
-import SignIn from "/src/pages/sign/SignIn";
-import Index from "/src/pages";
-import MyTestResult from "/src/pages/testResults/MyTestResult";
-import AllTestResult from "/src/pages/testResults/AllTestResult";
+import SignUp from "@/pages/sign/SignUp";
 import { LoginProtectedRoute } from "./LoginProtectedRoute";
 import { NotLoginProtectedRoute } from "./NotLoginProtectedRoute";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
-import Test from "/src/pages/test/Test";
-import Profile from "/src/pages/profile/Profile";
+import SignIn from "@/pages/sign/SignIn";
+import Index from "@/pages/Index"; // index 지워보기
+import MyTestResult from "@/pages/testResults/MyTestResult";
+import AllTestResult from "@/pages/testResults/AllTestResult";
+import Test from "@/pages/test/Test";
+import Profile from "@/pages/profile/Profile";
+
+// import Index from "/src/pages";
+// import MyTestResult from "/src/pages/testResults/MyTestResult";
+// import AllTestResult from "/src/pages/testResults/AllTestResult";
+// import { LoginProtectedRoute } from "./LoginProtectedRoute";
+// import { NotLoginProtectedRoute } from "./NotLoginProtectedRoute";
+// import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+// import Test from "/src/pages/test/Test";
+// import Profile from "/src/pages/profile/Profile";
+// import SignUp from "@/pages/sign/SignUp";
 
 const Routes = () => {
   const notAuthenticatedOnly = [
