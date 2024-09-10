a553643ab834a2456a593c333335d32f068cffdc
LeeJY97 <ijoonyeol97@gmail.com>
2024-09-10 22:18:23 +0900

    [test] vercel

--- src/routes/TestIndex.jsx
+++ src/routes/TestIndex.jsx
@@ -3,21 +3,11 @@ import { LoginProtectedRoute } from "./LoginProtectedRoute";
 import { NotLoginProtectedRoute } from "./NotLoginProtectedRoute";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import SignIn from "@/pages/sign/SignIn";
-import Index from "@/pages"; // index 지워보기
 import MyTestResult from "@/pages/testResults/MyTestResult";
 import AllTestResult from "@/pages/testResults/AllTestResult";
 import Test from "@/pages/test/Test";
 import Profile from "@/pages/profile/Profile";
-
-// import Index from "/src/pages";
-// import MyTestResult from "/src/pages/testResults/MyTestResult";
-// import AllTestResult from "/src/pages/testResults/AllTestResult";
-// import { LoginProtectedRoute } from "./LoginProtectedRoute";
-// import { NotLoginProtectedRoute } from "./NotLoginProtectedRoute";
-// import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
-// import Test from "/src/pages/test/Test";
-// import Profile from "/src/pages/profile/Profile";
-// import SignUp from "@/pages/sign/SignUp";
+import Index from "@/pages/Index";
 
 const Routes = () => {
   const notAuthenticatedOnly = [
